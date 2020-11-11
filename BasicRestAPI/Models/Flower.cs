ce13d56192f9e26fb6de7f88afa224d1c582391e
Bram Theunis <bramtheunis1997@gmail.com>
2020-11-11 06:53:52 +0100

    Added flower model

---
+++ BasicRestAPI/Models/Flower.cs
@@ -0,0 +1,10 @@
+namespace Architecture.Models
+{
+    public class Flower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; } 
+    }
+}
